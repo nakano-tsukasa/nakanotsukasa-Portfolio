e318039aa71660ac7ab8467ada37f2a86467a52d
nakano <nakanotsukasa929@gmail.com>
2024-09-03 17:04:10 +0900

    import dotenv

--- utils/chat_gpt.py
+++ utils/chat_gpt.py
@@ -1,11 +1,13 @@
 import os
 from openai import OpenAI
 import json
+from dotenv import load_dotenv
 
 def generate_summary_via_chatgpt(latest_d_summary_text,summary_text):
 
     user_message = f"Please summarize these two texts.Please answer in one sentence, no headings.:\n\nText 1:{latest_d_summary_text}\n\nText 2:{summary_text}"
 
+    load_dotenv()
     my_api_key = os.getenv("OPENAI_API_KEY")
     if not my_api_key:
         print("Error: OPENAI_API_KEY environment variable is not set.")
@@ -25,7 +27,6 @@ def generate_summary_via_chatgpt(latest_d_summary_text,summary_text):
         )
         ai_generated_summary_pre = json.loads(responce.model_dump_json())
         ai_generated_summary = ai_generated_summary_pre['choices'][0]['message']['content']
-        print(ai_generated_summary)
         return ai_generated_summary
     except Exception as e:
         print(f"Unexpected Error: {e}")
